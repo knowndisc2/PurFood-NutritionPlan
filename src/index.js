57fc070f3195dceba7ef16287e80a124eb67849f
wikzAM <anubhav.anuboss@gmail.com>
2025-09-20 15:52:45 -0400

    uncles I made styling ebtter and shi and I took away a css fiel lmao

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Auth from "./Auth";
+import "./index.css"; // Tailwind CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Auth />);
