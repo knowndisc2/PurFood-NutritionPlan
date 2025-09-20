8640ffbb076df4f78b3e68a8f204d98292988fbf
knowndisc2 <sun.pauly2@gmail.com>
2025-09-20 16:45:24 -0400

    something cool

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_mL7IrpkpLUjywnGmOxLDwO85a-11vbM",
@@ -16,13 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
 
-export {
-  db, // Export the database instance
-  auth,
-  provider,
-  signInWithPopup,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
-};
+export { auth, provider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword };
